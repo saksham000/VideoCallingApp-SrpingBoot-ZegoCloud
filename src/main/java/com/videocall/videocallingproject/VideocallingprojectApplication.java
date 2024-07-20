63067ff9b6b0923a8c08e6dee731ecf1cdb9e34d
Saksham Sharma <sakshamshankyan7@gmail.com>
2024-07-20 22:36:30 +0530

    initial commit

---
+++ src/main/java/com/videocall/videocallingproject/VideocallingprojectApplication.java
@@ -0,0 +1,13 @@
+package com.videocall.videocallingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideocallingprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideocallingprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/videocall/videocallingproject/VideocallingprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videocall.videocallingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideocallingprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
